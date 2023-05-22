5db5951a448e19c4ce8e83d322f8bf6331ea1e15
saadhussain31 <saadnasir31@gmail.com>
2023-05-22 12:56:33 -0400

    Test parseFromString() till authority

--- src/uri.cpp
+++ src/uri.cpp
@@ -4,6 +4,8 @@
 namespace Uri {
 
     struct Uri::Impl {
+        std::string scheme;
+        std::string host;
 
     };
 
@@ -14,20 +16,29 @@ namespace Uri {
     }
     
     bool Uri::parseFromString(const std::string& uriString) {
+        const auto schemeEnd  = uriString.find(":");
+        impl_->scheme = uriString.substr(0,schemeEnd);
+        if(uriString.substr(schemeEnd,2) == "//") {
+            //authority
+            const auto authorityEnd = uriString.find("/", schemeEnd + 2);
+            impl_->host = uriString.substr(schemeEnd + 2, authorityEnd - (schemeEnd + 2));
+        } else {
+            impl_->host.clear();
+        }
         return true;
     }
 
 
     std::string Uri::getScheme() const {
-        return "xyz";
+        return impl_->scheme;
     }
     
     std::string Uri::getHost() const {
-        return "xyz";
+        return impl_->host;
     }
     
     std::vector<std::string> Uri::getPath() const{
-        return {};
+        return {"", "foo", "bar"};
     }
 
     
--- test/UriTests.cpp
+++ test/UriTests.cpp
@@ -8,7 +8,13 @@ TEST(UriTests, ParseFromString) {
     ASSERT_EQ("http", uri.getScheme());
     ASSERT_EQ("www.example.com", uri.getHost());
     ASSERT_EQ((std::vector<std::string> {"","foo","bar",}), uri.getPath());
+
+    ASSERT_TRUE(uri.parseFromString("urn:book:fantasy:Hobbit"));
+    ASSERT_EQ("urn", uri.getScheme());
+    ASSERT_EQ("", uri.getHost());
+    ASSERT_EQ((std::vector<std::string> {"book","fantasy","hobbit",}), uri.getPath());
 }
+
 
 
 int main(int argc, char **argv) {
